324c702b8b915b15d3104c96188607c4a38709ef
Martin Scharm <martin@binfalse.de>
2021-01-11 00:25:50 +0100

    minimal restructuring

--- webextension/popup.js
+++ webextension/popup.js
@@ -56,47 +56,41 @@
 	var _gotSettings = function(settings) {
 		if (settings.api && settings.signature) {
 			var _haveTab = function(tabs) {
-        var long_url = tabs[0].url;
-        
-        browser.runtime.sendMessage({method: "getLinkTarget"}).then (function (response) {
-          if (response.linkTarget) {
-            long_url = response.linkTarget;
-            updateSource(long_url);
-          }
-        }, function (error) {updateError ("Communication error within the extension!", communicationErrorMsg);});
-        
-        
-				updateSource(long_url);
 				
-				
-				document.getElementById('admin').addEventListener(
-					'click',
-					function(se) {
-						window.open(settings.api + "admin/");
+				browser.runtime.sendMessage({method: "getLinkTarget"}).then (function (response) {
+					var long_url = tabs[0].url;
+					if (response.linkTarget) {
+						long_url = response.linkTarget;
 					}
-				);
-				
-				if (settings.keyword) {
-					browser.runtime.sendMessage({method: "getSelection"}).then (function (response) {
-						document.getElementById('keyword').value = response.selection;
-					}, function (error) {updateError ("Communication error within the extension!", communicationErrorMsg);});
+					updateSource(long_url);
 					
-					updateResult("", "Waiting for keyword...");
-					document.getElementById('keyword_submit').addEventListener(
+					document.getElementById('admin').addEventListener(
 						'click',
 						function(se) {
-							shorten (settings, long_url, document.getElementById('keyword').value);
+							window.open(settings.api + "admin/");
 						}
 					);
-				} else {
-					var keywordrow = document.getElementById('keyword_row');
-					keywordrow.parentNode.removeChild(keywordrow);
-					updateResult("", "Working...");
-					shorten (settings, long_url);
-				}
-				
-				
-				
+					
+					if (settings.keyword) {
+						browser.runtime.sendMessage({method: "getSelection"}).then (function (response) {
+							document.getElementById('keyword').value = response.selection;
+						}, function (error) {updateError ("Communication error within the extension!", communicationErrorMsg);});
+						
+						updateResult("", "Waiting for keyword...");
+						document.getElementById('keyword_submit').addEventListener(
+							'click',
+							function(se) {
+								shorten (settings, long_url, document.getElementById('keyword').value);
+							}
+						);
+					} else {
+						var keywordrow = document.getElementById('keyword_row');
+						keywordrow.parentNode.removeChild(keywordrow);
+						updateResult("", "Working...");
+						shorten (settings, long_url);
+					}
+					
+				}, function (error) {updateError ("Communication error within the extension!", communicationErrorMsg);});
 			};
 			var _tabQueryError = function(error) {
 				updateSource('Cannot get current tab URL!');
